9f93ffc92f73104e3adaf95286b8a26eaf0cfb01
haejin park <jelly12paw@gmail.com>
2025-04-08 14:42:24 +0900

    init: setting project

---
+++ HabitFlow/HabitFlow/HabitFlowApp.swift
@@ -0,0 +1,20 @@
+//
+//  HabitFlowApp.swift
+//  HabitFlow
+//
+//  Created by Haejin Park on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitFlowApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
