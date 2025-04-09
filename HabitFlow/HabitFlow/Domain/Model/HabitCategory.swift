e84b000ef5037991fe42f4a75974445cb424f8c5
haejin park <jelly12paw@gmail.com>
2025-04-09 18:11:05 +0900

    chore: redefine habitmodel

---
+++ HabitFlow/HabitFlow/Domain/Model/HabitCategory.swift
@@ -0,0 +1,26 @@
+//
+//  HabitCategory.swift
+//  HabitFlow
+//
+//  Created by Haejin Park on 4/9/25.
+//
+
+public enum HabitCategory: String, CaseIterable, Identifiable {
+    case healthyIt
+    case canDoIt
+    case moneyIt
+    case greenIt
+    case myMindIt
+
+    public var id: String { rawValue }
+
+    public var displayName: String {
+        switch self {
+        case .healthyIt: return "헬시잇"
+        case .canDoIt: return "할수잇"
+        case .moneyIt: return "머니잇"
+        case .greenIt: return "그린잇"
+        case .myMindIt: return "내맘잇"
+        }
+    }
+}
--- HabitFlow/HabitFlow/Presentation/View/HabitAddEditView.swift
+++ HabitFlow/HabitFlow/Presentation/View/HabitAddEditView.swift
@@ -44,23 +44,3 @@ struct HabitAddEditView: View {
 #Preview {
     HabitAddEditView()
 }
-
-public enum HabitCategory: String, CaseIterable, Identifiable {
-    case healthyIt
-    case canDoIt
-    case moneyIt
-    case greenIt
-    case myMindIt
-
-    public var id: String { rawValue }
-
-    public var displayName: String {
-        switch self {
-        case .healthyIt: return "헬시잇"
-        case .canDoIt: return "할수잇"
-        case .moneyIt: return "머니잇"
-        case .greenIt: return "그린잇"
-        case .myMindIt: return "내맘잇"
-        }
-    }
-}
