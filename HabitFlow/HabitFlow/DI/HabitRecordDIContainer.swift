4fc279a3da0a276634aed05cbd0f590698dfafe5
haejin park <jelly12paw@gmail.com>
2025-04-16 16:04:43 +0900

    feat: Add HabitRecordDIContainer

---
+++ HabitFlow/HabitFlow/DI/HabitRecordDIContainer.swift
@@ -0,0 +1,44 @@
+//
+//  HabitRecordDIContainer.swift
+//  HabitFlow
+//
+//  Created by Haejin Park on 4/16/25.
+//
+
+import Foundation
+
+final class HabitRecordDIContainer {
+
+    // MARK: - CoreData Storage
+    private let storage: HabitRecordCoreDataStorage
+
+    // MARK: - Repository
+    private let repository: HabitRecordRepository
+
+    // MARK: - UseCases
+    private let addHabitRecordUseCase: AddHabitRecordUseCase
+    private let deleteHabitRecordUseCase: DeleteHabitRecordUseCase
+    private let updateHabitRecordUseCase: UpdateHabitRecordUseCase
+    private let fetchHabitRecordsByHabitIdUseCase: FetchHabitRecordsByHabitIdUseCase
+
+    // MARK: - Init
+    init() {
+        self.storage = HabitRecordCoreDataStorage()
+        self.repository = HabitRecordRepositoryImpl(storage: storage)
+
+        self.addHabitRecordUseCase = DefaultAddHabitRecordUseCase(repository: repository)
+        self.deleteHabitRecordUseCase = DefaultDeleteHabitRecordUseCase(repository: repository)
+        self.updateHabitRecordUseCase = DefaultUpdateHabitRecordUseCase(repository: repository)
+        self.fetchHabitRecordsByHabitIdUseCase = DefaultFetchHabitRecordsByHabitIdUseCase(repository: repository)
+    }
+
+    // MARK: - ViewModel Factory
+    func makeHabitRecordViewModel(habitId: UUID) -> HabitRecordViewModel {
+        return HabitRecordViewModel(
+            fetchHabitRecordsByHabitIdUseCase: fetchHabitRecordsByHabitIdUseCase,
+            addHabitRecordUseCase: addHabitRecordUseCase,
+            updateHabitRecordUseCase: updateHabitRecordUseCase,
+            deleteHabitRecordUseCase: deleteHabitRecordUseCase
+        )
+    }
+}
